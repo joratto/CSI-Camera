248107f169419de2bd0c39c0154a382b53fa710b
Joaquim Dickson <47600979+joratto@users.noreply.github.com>
2025-08-27 21:50:46 +0100

    basic stereoscopic logic implementation
    untested!

--- diopsid.py
+++ diopsid.py
@@ -99,14 +99,15 @@ Default 1920x1080
 camera_width = 1920
 camera_height = 1080
 
+interocular_distance = 100 # (mm)
+horizontal_fov = 160 # (degrees)
+pixel_angular_width = np.deg2rad(horizontal_fov) / camera_width # (radians)
+
 center_x = camera_width // 2
 center_y = camera_height // 2
 
-def read_pixel(frame, x, y):   
-    return frame[y, x]
-
-window_width = 30 # (pixels)
-window_height = 30 # (pixels)
+window_width = 80 # (pixels)
+window_height = 80 # (pixels)
 
 scope_width = 800
 scope_height = 800
@@ -114,12 +115,23 @@ scope_height = 800
 scope_xlim = (center_x - scope_width//2, center_x + scope_width//2)
 scope_ylim = (center_y - scope_height//2, center_y + scope_height//2)
 
-epipolar_lines = camera_height // window_height
+epipolar_lines = scope_height // window_height
+columns = scope_width // window_width
 
 # display stuff only
 screen_width = 500
 screen_height = 500
 screen_ratio = (screen_width//camera_width, screen_height//camera_height)
+
+def read_pixel(frame, x, y):   
+    return frame[y, x]
+
+def read_window(frame, x_left, y_top, window_dims=(window_width, window_height)):
+    window = np.zeros(window_dims)
+    for x in range(x_left, x_left + window_dims[0]):
+        for y in range(y_top - window_dims[1], y_top):
+            window[x][y] = read_pixel(frame, x, y)
+    return window
 
 def normalise(window):
     # how to normalise an individual window
@@ -130,20 +142,22 @@ def normalise(window):
             sumsquares += window[wi, hi]**2
     return window / sumsquares
 
-def get_SSD(left_window, right_window):
+def get_SSD(template_window, comparison_window):
     # where left_window and right_window are NxM arrays of pixels,
     # and N is window_width and M is window_height.
 
-    w, h = left_window.shape
-    if (w, h) != right_window.shape:
+    # the actual cardinality of the window is not important. This function is symmetric.
+
+    w, h = template_window.shape
+    if (w, h) != comparison_window.shape:
         raise ValueError("Left and right windows must have the same shape")
     
     # First normalise (doing this double-counts a lot of pixels compared to normalising the whole image in one go, but it might make it easier to pick out patterns??):
     
-    left = normalise(left_window)
-    right = normalise(right_window)
+    template = normalise(template_window)
+    comparison = normalise(comparison_window)
     
-    difference = left - right
+    difference = template - comparison
 
     ssd = 0
     for wi in range(w):
@@ -152,17 +166,61 @@ def get_SSD(left_window, right_window):
 
     return ssd
 
-
-
+def scan_epipolar_line(left_image, right_image, template_top_left, window_dims=(window_width, window_height), scope_xlim=scope_xlim, scope_ylim=scope_ylim, left_first=True):
+    # window_top_left is [x, y] of the top left corner of the window providing the reference
     
-    left_sumsquares = left_sumsquares / (w * h)
-    right_sumsquares = right_sumsquares / (w * h)
-    
+    if left_first:
+        scan_xlim = (scope_xlim[0], template_top_left[0])
+        image_with_window = left_image
+        image_to_scan = right_image
+    else: # (i.e. right first)
+        scan_xlim = (template_top_left[0], scope_xlim[1] - window_dims[0])
+        image_with_window = right_image
+        image_to_scan = left_image
 
-    # Normalise:
-    left_window = left_window / 255.0
-    right_window = right_window / 255.0
+    template = read_window(image_with_window, window_top_left[0], window_top_left[1], window_dims=window_dims)
+    ssd_array = []
+    x_range = range(scan_xlim[0], scan_xlim[1])
+    #x_delta_array = x_range - scan_xlim[0]
+    #x_delta_array = np.arange(len(x_range))
+    for x in x_range:
+        comparison = read_window(image_to_scan, x, window_top_left[1], window_dims=window_dims)
+        ssd = get_SSD(template, comparison)
+        ssd_array.append(ssd)
+    return ssd_array, x_range
+
+def get_distance(ssd_array, x_range, interocular_distance, pixel_angular_width):
+    ssd_min = min(ssd_array)
+    x_match = x_range[ssd_array.index(ssd_min)]
+    x_delta_array = np.arange(len(x_range))
+    x_delta = x_delta_array[x_range.index(x_match)] # this seems silly
+    theta = x_delta * pixel_angular_width
+    distance = interocular_distance / (2*np.tan(theta/2))
+    return distance, x_match
     
+def get_distance_matrix(left_image, right_image, interocular_distance, pixel_angular_width, window_dims=(window_width, window_height), scope_xlim=scope_xlim, scope_ylim=scope_ylim, left_first=True):
+    distance_matrix = np.zeros((epipolar_lines, columns, 5))
+    template_top_left = (scope_xlim[0], scope_ylim[0]+window_dims[1])
+    for line in range(epipolar_lines):
+        for column in range(columns):
+            ssd_array, x_range = scan_epipolar_line(left_image, right_image, template_top_left, window_dims=window_dims, scope_xlim=scope_xlim, scope_ylim=scope_ylim, left_first=left_first)
+            distance, x_match = get_distance(ssd_array, x_range, interocular_distance, pixel_angular_width)
+            template_x = template_top_left[0] + window_dims[0] // 2
+            template_y = template_top_left[1] - window_dims[1] // 2
+            comparison_x = x_match + window_dims[0] // 2
+            comparison_y = template_top_left[1] - window_dims[1] // 2 # it's the same, because of the epipolar lines constraint!
+            distance_matrix[line][column] = [template_x, template_y, comparison_x, comparison_y, distance]
+
+            template_top_left[0] += window_dims[0]
+        template_top_left[1] += window_dims[1]
+    
+    return distance_matrix
+
+# TODO: 
+# - test that scope boxes are still plotting in the middle
+# - work out how to disregard insignificant SSD minima
+# - implement slight vertical shifts +/- epipolar lines to account for errors in the camera alignment
+# - implement a way to check SSD in different colour channels independently, and then combine
 
 
 def gstreamer_pipeline(
@@ -205,6 +263,7 @@ def run_cameras():
             flip_method=0,
             display_width=screen_width,
             display_height=screen_height,
+            framerate=2, # frames per second
         )
     )
     left_camera.start()
@@ -218,6 +277,7 @@ def run_cameras():
             flip_method=0,
             display_width=screen_width,
             display_height=screen_height,
+            framerate=2, # frames per second
         )
     )
     right_camera.start()
@@ -230,16 +290,24 @@ def run_cameras():
             while True:
                 _, left_image = left_camera.read()
                 _, right_image = right_camera.read()
+
+                left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY)
+                right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY)
+
                 # Use numpy to place images next to each other
                 camera_images = np.hstack((left_image, right_image)) 
                 
+                # Draw scope boxes
                 cv2.rectangle(left_image, (scope_xlim[0]*screen_ratio[0], scope_ylim[0]*screen_ratio[1]), (scope_xlim[1]*screen_ratio[0], scope_ylim[1]*screen_ratio[1]), (255, 0, 0), 8)
                 cv2.rectangle(right_image, (scope_xlim[0]*screen_ratio[0], scope_ylim[0]*screen_ratio[1]), (scope_xlim[1]*screen_ratio[0], scope_ylim[1]*screen_ratio[1]), (255, 0, 0), 8)
 
-                ssd = get_SSD(left_window, right_window)
-                print(ssd)
-                
-                
+                # Get distance matrix
+                distance_matrix = get_distance_matrix(left_image, right_image, interocular_distance, pixel_angular_width, window_dims=(window_width, window_height), scope_xlim=scope_xlim, scope_ylim=scope_ylim, left_first=True)
+
+                # Print distance matrix
+                print('\n\n\n')
+                print(distance_matrix)
+
                 # Check to see if the user closed the window
                 # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
                 # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
